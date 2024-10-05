8254c57742c8331c1015e0b4ad807938b7890eb6
Pinky <ishratjahanpinky2@gmail.com>
2024-10-06 04:01:21 +0600

    style update form submite but get function need to update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
     <div className="home">
       <h1>Collaborative Storytelling</h1>
       <p>Join the community to create and contribute to ongoing stories!</p>
-        <Link to="/stories" style={{ margin: "10px", padding: "10px", backgroundColor: "#007BFF", color: "white", textDecoration: "none", borderRadius: "5px" }}>
+        <Link to="/stories" style={{ margin: "10px", padding: "10px", backgroundColor: "#4A4A8A", color: "white", textDecoration: "none", borderRadius: "5px" }}>
             View Stories
         </Link>
 
-        <Link to="/upload" style={{ margin: "10px", padding: "10px", backgroundColor: "#007BFF", color: "white", textDecoration: "none", borderRadius: "5px" }}>
+        <Link to="/upload" style={{ margin: "10px", padding: "10px", backgroundColor: "#4A4A8A", color: "white", textDecoration: "none", borderRadius: "5px" }}>
             Upload a Contribution
         </Link>
     </div>
